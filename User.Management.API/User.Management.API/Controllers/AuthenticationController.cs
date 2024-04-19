c79fa74b7ad5eca176065035baf903ae73693125
ntuan3324 <ntuan3324@gmail.com>
2024-04-19 21:01:33 +0700

    first commit

---
+++ User.Management.API/User.Management.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using User.Management.API.Models;
+using User.Management.API.Models.Authentication.Login;
+using User.Management.API.Models.Authentication.SignUp;
+
+namespace User.Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public object SecurityAlgorithm { get; private set; }
+
+        public AuthenticationController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            //Check user exist
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist != null) 
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User aldready exist!" });
+            }
+            //Add the user in the database
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username
+            };
+            if ( await _roleManager.RoleExistsAsync(role) )
+            {
+                var result = await _userManager.CreateAsync(user, registerUser.Password);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        Console.WriteLine($"Error code: {error.Code}, Description: {error.Description}");
+                    }
+                    return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "User failed to created!" });
+                }
+                // Add role to the user...
+                await _userManager.AddToRoleAsync(user, role);
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response { Status = "Success", Message = "User created successfully!" });     
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        new Response { Status = "Error", Message = "This role doesn't exist!" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            //checking the user
+            var user = await _userManager.FindByNameAsync(loginModel.Username);
+            //checking the password
+            if (user != null &&  await _userManager.CheckPasswordAsync(user,loginModel.Password))
+            {
+                //claimlist creation
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+                //add roles to the list
+                var userRoles = await _userManager.GetRolesAsync(user);
+                foreach (var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                //generate the token with the claims
+                var jwtToken = GetToken(authClaims);
+                //returning the token
+                return Ok(new 
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expiration = jwtToken.ValidTo
+                });
+            }
+            return Unauthorized();
+
+        }
+        private JwtSecurityToken GetToken(List<Claim> authClaims) 
+        {
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience : _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+            return token;
+        }
+    }
+}
---
+++ User.Management.API/User.Management.API/Migrations/20240418113051_RolesSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace User.Management.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2dbc1f52-70c0-486a-b186-5b1f1a584c21", "1", "Admin", "Admin" },
+                    { "c5aa0337-0710-4cf4-9c11-933c89f773a4", "2", "User", "User" },
+                    { "d2fbb2c5-3550-4614-b2f3-6bfa77fd0cc2", "3", "HR", "HR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2dbc1f52-70c0-486a-b186-5b1f1a584c21");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c5aa0337-0710-4cf4-9c11-933c89f773a4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d2fbb2c5-3550-4614-b2f3-6bfa77fd0cc2");
+        }
+    }
+}
